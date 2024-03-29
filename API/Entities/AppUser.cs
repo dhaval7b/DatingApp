c0ed05ec2f06af297eea74cbc1c6913a0640aa18
dhaval7b <dhavalrbhimani@gmail.com>
2024-03-29 15:56:07 -0400

    Add project files.

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Entities
+{
+    public class AppUser
+    {
+        
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+
+    }
+}
