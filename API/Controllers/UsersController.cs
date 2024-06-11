e7ea29d380fb12865642db1c51fa520cce23cf9e
dhavalrbhimani <logan7bhimani@gmail.com>
2024-06-11 15:30:31 -0400

    add login and register

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context) {
+            _context = context;
+        }
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(long id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return user;
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API/DTOs/RegisterRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTOs;
+
+public class AccountRequestDTO
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string UserEmail { get; set; }
+}
